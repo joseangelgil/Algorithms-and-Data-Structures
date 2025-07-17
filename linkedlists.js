a54acf9aa919d3650ac32be75898e8d8b5482d4d
joseangelgil <180407943+joseangelgil@users.noreply.github.com>
2025-07-17 09:28:54 +0200

    Add problem 6, zipper lists

--- linkedlists.js
+++ linkedlists.js
@@ -20,7 +20,6 @@ class Node {
 // const printLinkedList = (head) => {
 //   let current = head;
 //   while(current !== null) {
-//     console.log(current.val)
 //     current = current.next
 //   }
 // };
@@ -28,7 +27,6 @@ class Node {
 // TRAVERSE RECURSIVELY
 // const printLinkedList = (head) => {
 //   if (head === null) return;
-//   console.log(head.val)
 //   printLinkedList(head.next)
 // }
 
@@ -65,16 +63,16 @@ class Node {
 
 // PROBLEM 2: SUM ELEMENTS OF A LINKED LIST
 
-const a = new Node(2);
-const b = new Node(8);
-const c = new Node(3);
-const d = new Node(-1);
-const e = new Node(7);
+// const a = new Node(2);
+// const b = new Node(8);
+// const c = new Node(3);
+// const d = new Node(-1);
+// const e = new Node(7);
 
-a.next = b;
-b.next = c;
-c.next = d;
-d.next = e;
+// a.next = b;
+// b.next = c;
+// c.next = d;
+// d.next = e;
 
 // ITERATIVELY
 // const sumNodes = (head) => {
@@ -181,6 +179,78 @@ d.next = e;
 // }
 
 // console.log(reverseLinkedList(a))
+
+
+
+
+// PROBLEM 6: ZIPPER LISTS
+
+// const a = new Node('a')
+// const b = new Node('b')
+// const c = new Node('c')
+// const d = new Node('d')
+// const e = new Node('e')
+// const f = new Node('f')
+// a.next = b;
+// b.next = c;
+// c.next = d;
+// d.next = e;
+// e.next = f;
+// // a --> b --> c --> d --> e --> f
+
+// const x = new Node('x')
+// const y = new Node('y')
+// const z = new Node('z')
+// x.next = y;
+// y.next = z;
+// x --> y --> z
+
+// zipperLists(a, x);
+// a --> x --> b --> y --> c --> z --> d --> e --> f
+
+
+// ITERATIVELY
+// const zipperLists = (head1, head2) => {
+//   let tail = head1;
+//   let current1 = head1.next;
+//   let current2 = head2;
+//   let count = 0;
+
+//   while(current1 !== null && current2 !== null) {
+//     if(count % 2 === 0) {
+//       tail.next = current2;
+//       current2 = current2.next;
+//     } else {
+//       tail.next = current1;
+//       current1 = current1.next;
+//     }
+//     tail = tail.next
+//     count++
+//   }
+
+//   if(current1 !== null) tail.next = current1;
+//   if(current2 !== null) tail.next = current2;
+
+//   return head1;
+// }
+
+// RECURSIVELY
+// const zipperLists = (head1, head2) => {
+//   if(head1 === null && head2 === null) return null;
+//   if(head1 === null) return head2;
+//   if(head2 === null) return head1;
+
+//   const next1 = head1.next;
+//   const next2 = head2.next;
+
+//   head1.next = head2;
+//   head2.next = zipperLists(next1, next2);
+
+//   return head1;
+// }
+// console.log(zipperLists(a, x))
+
+
 
 
 
